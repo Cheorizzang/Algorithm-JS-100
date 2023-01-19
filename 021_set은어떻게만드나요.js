414ea7609b026d5c19cf0f90907c040d10ea561b
Cheorizzang <ouov2gg@gmail.com>
2023-01-19 23:10:33 +0900

    21일차

---
+++ 021_set은어떻게만드나요.js
@@ -0,0 +1,15 @@
+// 문제 : 다음 중 set을 만드는 방법으로 올바른 것을 모두 고르시오.
+
+// 1)  var x = {1, 2, 3, 5, 6, 7};
+// 2)  var x = {}; -> 객체 생성 리터럴
+// 3)  var x = new Set('javascript'); -> (O)
+// 4)  var x = new Set(range(5)); -> 알 수 없는 메서드
+// 5)  var x = new Set(); -> (O)
+
+// set : 중복되지 않는 데이터를 저장하는 데이터 구조
+
+// const list = [1, 2, 3, 4, 5, 6, 7]
+// let setVal = new Set(list)
+// setVal.add(10) -> 맨 끝에 10 추가
+// setVal.delete(10) -> 10 삭제
+// setVal.has(10) -> boolean 반환
