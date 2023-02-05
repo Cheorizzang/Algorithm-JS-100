398a7a9c6082e9ced7bd98ed400ac5e6dcd17bcd
Cheorizzang <ouov2gg@gmail.com>
2023-02-05 23:12:20 +0900

    37일차

--- 037_반장선거.js
+++ 037_반장선거.js
@@ -13,8 +13,17 @@ for (let index in array) {
 
 winner = Object.keys(result).reduce(function (a, b) {
     return result[a] > result[b] ? a : b;
-}); // reduce는 결과값으로 나온게 다시 a가 되는 식으로 중첩됨
+});
+// Object.keys(obj) : obj라는 객체의 키 값만 배열로 반환
+// reduce의 첫번째 파라미터는 누적값, 두번째는 현재값 -> 결과값으로 나온게 다시 a가 되는 식으로 중첩됨
 
 console.log(`${winner}(이)가 총 ${result[winner]}표로 반장이 되었습니다.`);
 
-// Object.keys(obj) : obj라는 객체의 키 값만 배열로 반환
+// forof vs forin : 배열 등과 함께 사용
+for (const 요소 of object) {
+    // 배열의 요소를 순회
+}
+
+for (const 인덱스 in object) {
+    // 배열의 인덱스를 순회
+}
